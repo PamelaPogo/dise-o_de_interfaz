e566cf03f8675f780590c52777e0958b6531272d
Pamela Pogo <pme.pogo@yavirac.edu.ec>
2024-11-28 15:12:04 -0500

    Matriz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { CommonModule } from '@angular/common'; 
-=======
 import { RouterOutlet } from '@angular/router';
->>>>>>> 8a0782b8c497fdf2343b97cb4dd40b2769b467cd
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./app.component.css'],
-  standalone: true, 
-  imports: [CommonModule]  
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'matriz';
-=======
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'Matrices';
->>>>>>> 8a0782b8c497fdf2343b97cb4dd40b2769b467cd
+  title = 'gridmatrizv1';
 }
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -4,11 +4,7 @@ import { appConfig } from './app.config';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-<<<<<<< HEAD
     provideServerRendering()
-=======
-    provideServerRendering(),
->>>>>>> 8a0782b8c497fdf2343b97cb4dd40b2769b467cd
   ]
 };
 
